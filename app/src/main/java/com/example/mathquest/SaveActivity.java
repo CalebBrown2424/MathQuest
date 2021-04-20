e1123807940f0c173573aefdb724f8b0d465d278
juicysmoorth <mudkipmastermind@gmail.com>
2021-04-20 10:46:51 -0700

    activities created

--- app/src/main/java/com/example/mathquest/SaveActivity.java
+++ app/src/main/java/com/example/mathquest/SaveActivity.java
@@ -1,14 +1,13 @@
 package com.example.mathquest;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class SaveActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_saves);
     }
 }
